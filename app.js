2eac1201bce0cf68b5ebd8065c2b517e3b306aa7
Alexandre Amourdom <alexandre.amourdom@gmail.com>
2022-10-04 14:53:14 +0200

    gestion des inputs et du formulaire

---
+++ app.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("invalid", handleValidation);
+  input.addEventListener("input", handleValidation);
+});
+
+function handleValidation(e) {
+  if (e.type === "invalid") {
+    e.target.setCustomValidity("Ce champ ne peut être vide");
+  } else if (e.type === "input") {
+    e.target.setCustomValidity("");
+  }
+}
+
+const cookieForm = document.querySelector("form");
+cookieForm.addEventListener("submit", handleForm);
+
+function handleForm(e) {
+  e.preventDefault();
+
+  const newCookie = {};
+
+  inputs.forEach((input) => {
+    const nameAttribute = input.getAttribute("name");
+    newCookie[nameAttribute] = input.value;
+  });
+  newCookie.expire = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+
+  createCookie(newCookie);
+}
+
+function createCookie(newCookie) {}
