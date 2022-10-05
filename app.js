174553be5180abec11ce8a46d06095eaebd6caf6
Alexandre Amourdom <alexandre.amourdom@gmail.com>
2022-10-05 15:00:31 +0200

    gestion des cookies, creation, modif, reset

--- app.js
+++ app.js
@@ -25,9 +25,46 @@ function handleForm(e) {
     const nameAttribute = input.getAttribute("name");
     newCookie[nameAttribute] = input.value;
   });
-  newCookie.expire = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+  newCookie.expires = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
 
+  cookieForm.reset();
   createCookie(newCookie);
 }
 
-function createCookie(newCookie) {}
+function createCookie(newCookie) {
+  if (doesCookieExist(newCookie.name)) {
+    createToast({ name: newCookie.name, state: "modifié", color: "orangered" });
+  } else {
+    createToast({ name: newCookie.name, state: "crée", color: "green" });
+  }
+  document.cookie = `${encodeURIComponent(newCookie.name)}=${encodeURIComponent(
+    newCookie.value
+  )};expires=${newCookie.expires.toUTCString()}`;
+}
+
+function doesCookieExist(name) {
+  //remplace les espaces par une chaine de caractère vide puis supp le (;)
+  const cookies = document.cookie.replace(/\s/g, "").split(";");
+  //supprime le (=) et garde uniquement la valeur de l'index [0] cad name
+  const onlyCookiesName = cookies.map((cookie) => cookie.split("=")[0]);
+
+  const cookiePresence = onlyCookiesName.find(
+    (cookie) => cookie === encodeURIComponent(name)
+  );
+
+  return cookiePresence;
+}
+
+const toastContainer = document.querySelector(".toast-container");
+
+function createToast({ name, state, color }) {
+  const toastInfo = document.createElement("p");
+  toastInfo.className = "toast";
+  toastInfo.textContent = `Cookie ${name} ${state}`;
+  toastInfo.style.backgroundColor = color;
+  toastContainer.appendChild(toastInfo);
+
+  setTimeout(() => {
+    toastInfo.remove();
+  }, 2500);
+}
